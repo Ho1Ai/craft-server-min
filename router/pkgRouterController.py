362bb5eb916f3657cef637b6822efab7e5b71402
Ho1Ai desktop <koekto33309@gmail.com>
2026-07-08 18:55:40 +0000

    adding some db work. Gonna continue tomorrow

--- router/pkgRouterController.py
+++ router/pkgRouterController.py
@@ -13,80 +13,90 @@ from models import default_models as models
 from service import pkgCountHash
 
 global_router = APIRouter(prefix='/api')
+#
+# @global_router.get('/pkg-info')
+# async def getPkgSingleInfo(request: Request, name):
+# 	#print(name)
+# 	data = await pkgInfo.pkgsInfo(name)
+# 	#print(data['version'], data["existence"])
+# 	return {"is_ok": True,
+# 			"status_code": 0,
+# 			"existence": data.get("existence"),
+# 			"version": data.get("version")
+# 			}
+#
 
-@global_router.get('/pkg-info')
-async def getPkgSingleInfo(request: Request, name):
-	#print(name)
-	data = await pkgInfo.pkgsInfo(name)
-	#print(data['version'], data["existence"])
-	return {"is_ok": True,
-			"status_code": 0,
-			"existence": data.get("existence"),
-			"version": data.get("version")
-			}
-
+@global_router.get('/check-pkg-existence')
+async def checkPkgExistence(request: Request):
+    result = await pkgGet.checkPkgExistence(request)
+    return {"existence": result}
 
 @global_router.get('/pkg-get')
 async def getPkgArchieve(request: Request):
-	pkg_name = request.headers["pkg_name"]
-	pkg_server_name_getter = await pkgGet.getPkgPath(pkg_name)
+    pkg_name = request.headers["pkg_name"]
+    pkg_server_name_getter = await pkgGet.getPkgPath(pkg_name)
 
-	if pkg_server_name_getter["existence"]:
-		if os.path.exists("./pkg_dir/" + pkg_server_name_getter['server_name']):
-			new_file = "./pkg_dir/"+pkg_server_name_getter['server_name']
-			return FileResponse(new_file, headers={"App-Pkg-Version": pkg_server_name_getter["version"]})
-		else:
-			return {"is_ok": False, "status_code": 11}
-	else:
-		return {"is_ok": False, "status_code": 11}
+    if pkg_server_name_getter["existence"]:
+        if os.path.exists(pkg_server_name_getter['server_name']):
+            pkg_len = os.path.getsize(pkg_server_name_getter.get['server_name'])
+            pkg_hash = pkgCountHash.countFileHash(pkg_server_name_getter['server_name'])
+            new_file = pkg_server_name_getter['server_name']
+            return FileResponse(new_file, headers={"App-Pkg-Version": pkg_server_name_getter["version"], "App-Pkg-Size": pkg_len, "App-Pkg-Hash": pkg_hash})
+        else:
+            return {"is_ok": False, "status_code": 11}
+    else:
+        return {"is_ok": False, "status_code": 11}
+#
+# @global_router.post('/pkg-post-test')
+# async def test_post (package_file: UploadFile = File(...)):
+#     contents = await package_file.read()
+#
+#     # pkg_count_hash = pkgCountHash.countFileHash(contents)
+#
+#     print(len(contents))
 
-@global_router.post('/pkg-post-test')
-async def test_post (package_file: UploadFile = File(...)):
-    contents = await package_file.read()
-
-    # pkg_count_hash = pkgCountHash.countFileHash(contents)
-
-    print(len(contents))
-
-@global_router.post('/add-package')
-async def createPackage(new_pkg_creation_instance: models.CreationModel = Body(...), package_file: UploadFile = File(...)):
-    hashed_pkg_update_key = pkgCountHash.pkgHexifier(new_pkg_creation_instance.pkg_update_key)
-    new_pkg_name = new_pkg_creation_instance.pkg_name.lower().replace(" ", "-")
-
-    new_pkg_server_path = new_pkg_creation_instance.pkg_name.lower()+'-'+new_pkg_creation_instance.pkg_version.lower()
-
-    contents = await package_file.read()
-    with open(new_pkg_server_path, 'wb') as f:
-        f.write(contents)
-
-    pkg_count_hash = pkgCountHash.countFileHash(contents)
-
-    print(pkg_count_hash, len(contents), hashed_pkg_update_key, new_pkg_server_path)
-
-    status = await pkgPost.createPackage(pkg_name=new_pkg_name,
-	    pkg_version=new_pkg_creation_instance.pkg_version,
-	    pkg_dependencies=new_pkg_creation_instance.pkg_dependencies,
-	    pkg_maintainer=new_pkg_creation_instance.pkg_maintainer,
-	    pkg_server_path=new_pkg_server_path,
-	    pkg_update_key=hashed_pkg_update_key,
-        pkg_hash=pkg_count_hash,
-        pkg_size=len(contents))
-    return status
+# @global_router.post('/add-package')
+# async def createPackage(new_pkg_creation_instance: models.CreationModel = Body(...), package_file: UploadFile = File(...)):
+#     hashed_pkg_update_key = pkgCountHash.pkgHexifier(new_pkg_creation_instance.pkg_update_key)
+#     new_pkg_name = new_pkg_creation_instance.pkg_name.lower().replace(" ", "-")
+#
+#     new_pkg_server_path = new_pkg_creation_instance.pkg_name.lower()+'-'+new_pkg_creation_instance.pkg_version.lower()
+#
+#     contents = await package_file.read()
+#     with open(new_pkg_server_path, 'wb') as f:
+#         f.write(contents)
+#
+#     pkg_count_hash = pkgCountHash.countFileHash(contents)
+#
+#     print(pkg_count_hash, len(contents), hashed_pkg_update_key, new_pkg_server_path)
+#
+#     status = await pkgPost.createPackage(pkg_name=new_pkg_name,
+# 	    pkg_version=new_pkg_creation_instance.pkg_version,
+# 	    pkg_dependencies=new_pkg_creation_instance.pkg_dependencies,
+# 	    pkg_maintainer=new_pkg_creation_instance.pkg_maintainer,
+# 	    pkg_server_path=new_pkg_server_path,
+# 	    pkg_update_key=hashed_pkg_update_key,
+#         pkg_hash=pkg_count_hash,
+#         pkg_size=len(contents))
+#     return status
 
 @global_router.post("/request-pkg-add")
 async def requestPkgAdd(request: models.RequestPkgAdd):
     result = {"is_ok": True, "status_code": 0}
     return result
 
+@global_router.get("/get-pkg-dependencies")
+async def getPkgDependencies(name: str):
+    result = pkgGet.resolveFullDependenciesList(name)
+    return {"dependencies": result}
+
 @global_router.post('/get-pkg-data')
 async def getPkgData(pkg_list: models.RequestList):
-    full_dependencies_list = await pkgGet.resolveFullDependenciesList(pkg_list.package_list)
-    result = {"packages": [
-        {
-            "name": "test",
-            "version": "0.0.1",
-            "pkg_size": 0,
-            "pkg_hash": ""
-        }
-    ]}
+    # full_dependencies_list = await pkgGet.resolveFullDependenciesList(pkg_list.package_list)
+    final_array = []
+    for pkg in pkg_list.package_list:
+        pkg_data = await pkgGet.getPkgData(pkg)
+        final_array.append(pkg_data)
+
+    result = {"packages": final_array}
     return result
--- service/pkgGet.py
+++ service/pkgGet.py
@@ -1,4 +1,5 @@
 from db import db
+import os
 
 async def getPkgPath(pkg_name):
 	pool = await db.dbConnectionOpen()
@@ -14,41 +15,61 @@ async def getPkgPath(pkg_name):
 
 	if row:
 		package_name_getter_constructor["existence"] = True
-		package_name_getter_constructor["server_name"] = row["server_name"]
+		package_name_getter_constructor["server_name"] = row["pkg_server_path"]
 		package_name_getter_constructor["version"] = row["version"]
 	await conn.close()
 	await pool.close()
 
 	return package_name_getter_constructor 
 
-async def resolveFullDependenciesList(pkg_list):
+async def resolveFullDependenciesList(pkg_name):
     pool = await db.dbConnectionOpen()
     conn = await pool.acquire()
 
-    pkg_list_id = []
+    result_id = await conn.fetch("select dependencies from packages_list where name = $1", pkg_name)
 
-    for pkg in pkg_list:
-        pkg_list_id.append(await conn.fetchrow("select id from packages_list where name = $1", pkg))
+    result = []
 
-    pending = set(pkg_list_id)
-    result = set()
-    checked = set()
-
-    while pending:
-        current = pending.pop()
-
-        if current in checked:
-            continue
-
-        dependencies = await conn.fetchrow("select dependencies from packages_list where id = $1", current)
-
-        result.update(dependencies)
-        pending.update(dependencies)
-
-        checked.add(current)
-
-    if (result == checked):
-        print("all dependencies resolved")
+    for id in result_id:
+        new_obj = await conn.fetch("select name from packages_list where id = $1", id)
+        result.append(new_obj)
 
     await conn.close()
     await pool.close()
+
+    return result
+
+async def checkPkgExistence(pkg_name):
+    pool = await db.dbConnectionOpen()
+    conn = await pool.acquire()
+
+    result = False
+
+    pkg_instance = await conn.fetchrow("select * from packages_list where name = $1", pkg_name)
+
+    if pkg_instance:
+        result = True
+
+    await conn.close()
+    await pool.close()
+
+    return result
+
+async def getPkgData(pkg_name):
+    pool = await db.dbConnectionOpen()
+    conn = await pool.acquire()
+
+    result = {"name": "", "version": "", "pkg_size": 0}
+
+    pkg_full_row = await conn.fetchrow("select * from packages_list where name = $1", pkg_name)
+
+    if pkg_full_row:
+        result["id"] = pkg_full_row["id"]
+        result["name"] = pkg_full_row["name"]
+        result["version"] = pkg_full_row["version"]
+        result["pkg_size"] = os.path.getsize(pkg_full_row['pkg_server_path'])
+
+    await conn.close()
+    await pool.close()
+
+    return result
